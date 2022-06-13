9e01423d8d72d0966c063037f4f6cee510b072bc
msnkr <mikyle.designs@gmail.com>
2022-06-13 15:21:10 +0200

    .

--- portfo_project/portfo_app/urls.py
+++ portfo_project/portfo_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, about_me
 
 urlpatterns = [
     path('', home_page, name='home-page'),
+    path('about-me', about_me, name='about'),
 ]
--- portfo_project/portfo_app/views.py
+++ portfo_project/portfo_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'portfo_app/home-page.html')
+
+
+def about_me(request):
+    return render(request, 'portfo_app/about-me.html')
